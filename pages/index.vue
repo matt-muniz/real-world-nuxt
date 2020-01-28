38fee93d666fab391ddafa61235a67427d8cf2b1
Matt Muniz-silva <mattmuniz-silva@Matts-iMac.local>
2020-01-28 08:16:33 -0500

    api calls with axios

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,36 @@
 <template>
   <div>
     <h1>Events</h1>
+    <EventCard v-for="(event, index) in events" :key="index" :event="event" :data-index="index" />
   </div>
 </template>
 
 <script>
+import EventCard from '@/components/EventCard.vue'
+
 export default {
+  components: {
+    EventCard
+  },
   head() {
     return {
       title: 'Event listing'
     }
+  },
+  asyncData({ $axios, error }) {
+    return $axios
+      .get('http://localhost:3001/events')
+      .then(response => {
+        return {
+          events: response.data
+        }
+      })
+      .catch(e =>
+        error({
+          statusCode: 503,
+          message: 'Unable to fetch events at this time. Please try again'
+        })
+      )
   }
 }
 </script>
